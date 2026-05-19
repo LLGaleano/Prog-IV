dc7d14e00d95cec0ccb2e76161bc6097616dd935
Nicolas Calabrese <nicolas.calabrese915@gmail.com>
2026-05-19 00:12:24 -0300

    Conexion entre front y back terminada (edit y delete)

--- web/js/estudiantes.js
+++ web/js/estudiantes.js
@@ -1,14 +1,8 @@
-//Puse todas las variables aca arriba juntas para no tener que repetirlas. (Elementos del DOM)
-
-//Traemos el tbody para cargar los datos de los estudiantes
+/// ELEMENTOS DEL DOM (Los dejamos acá arriba para no repetirlo en cada función)
 const tabla = document.getElementById('tbody'); 
-//Recibimos los datos del usuario para hacer la busqueda
 const buscadorEstudiante = document.querySelector('.buscador'); 
-//Recibimos la ventana modal para editar los datos del estudiante
-const ventanaEdicion = document.getElementById('ventanaEditar');
-//Recibimos el formulario del modal 
+const ventanaEdicion = document.getElementById('ventanaEditar'); 
 const formEditarAlumno = document.getElementById('formEditarAlumno');
-//Recibimos el botón para cancelar la edición
 const btnCancelarEdicion = document.getElementById('btnCancelarEdicion');
 
 
@@ -26,6 +20,7 @@ const llenarTabla = (datos) =>{
         tabla.innerHTML = '<tr><td colspan="8" style="text-align: center; padding: 20px;">No se encontraron estudiantes con esa búsqueda.</td></tr>';
         return;
     }
+    //llenamos la tabla con los datos
     datos.forEach(estudiante => {
         const fila = document.createElement('tr');
         let classEstado = '';
@@ -92,7 +87,6 @@ tabla.addEventListener('click',async (evento) => {
 
         // fetch DELETE
         try {
-                // Fetch tipo DELETE 
                 const respuesta = await fetch(`http://localhost:3000/students/${idEstudiante}`, {
                     method: 'DELETE'
                 });
@@ -123,7 +117,6 @@ tabla.addEventListener('click',async (evento) => {
         const respuesta = await fetch(`http://localhost:3000/students/${idEstudiante}`,)
         if (respuesta.ok) {
             const estudiante = await respuesta.json();
-            //Llenamos los imput
                 document.getElementById('editId').value          = estudiante.id ;
                 document.getElementById('editDocumento').value   = estudiante.documento ;
                 document.getElementById('editNombres').value     = estudiante.nombres;
@@ -135,7 +128,6 @@ tabla.addEventListener('click',async (evento) => {
                     document.getElementById('editFechaNac').value = fechaLimpia;
                 }
                 document.getElementById('editActivo').value      = estudiante.activo;
-                // Mostramos el modal
                 ventanaEdicion.showModal();
                 } else {
                 alert("No se pudieron cargar los datos de este estudiante.");
@@ -148,29 +140,25 @@ tabla.addEventListener('click',async (evento) => {
     }
     });
     
-    // Agregamos un evento al formulario del modal para que al enviar los cambios, se haga un fetch PUT a la API
+    // Agregamos un evento al formulario para que al enviar los cambios, se haga un fetch PUT a la API
 
 if (formEditarAlumno) {
     formEditarAlumno.addEventListener('submit', async (evento) => {
-        // 1. Frenamos la recarga de la página
+        // Frenamos la recarga de la página
         evento.preventDefault(); 
-
-        // 2. Extraemos el ID que dejamos escondido
         const idEstudiante = document.getElementById('editId').value;
 
-        // 3. Armamos el JSON con los valores que el usuario acaba de tipear
+        // Armamos el JSON con los valores que el usuario acaba de ingresar
         const estudianteActualizado = {
             documento: document.getElementById('editDocumento').value,
             nombres: document.getElementById('editNombres').value,
             apellido: document.getElementById('editApellido').value,
             email: document.getElementById('editEmail').value,
             fecha_nacimiento: document.getElementById('editFechaNac').value,
-            // Convertimos el estado a número por si tu Back-end es estricto
             activo: parseInt(document.getElementById('editActivo').value) 
         };
-
+        //Enviamos con el put a la API
         try {
-            // 4. Mandamos el PUT
             const respuesta = await fetch(`http://localhost:3000/students/${idEstudiante}`, {
                 method: 'PUT',
                 headers: { 'Content-Type': 'application/json' },
@@ -179,16 +167,12 @@ if (formEditarAlumno) {
 
             if (respuesta.ok) {
                 alert('¡Estudiante actualizado con éxito!');
-                
-                // 5. Cerramos el modal mágicamente
                 ventanaEdicion.close(); 
-                
-                // 6. Recargamos la tabla en vivo para ver el cambio
                 const res = await fetch('http://localhost:3000/students');
                 const datosActualizados = await res.json();
                 llenarTabla(datosActualizados);
             } else {
-                alert('Hubo un problema al actualizar los datos en el servidor. ');//Revisá si el DNI está duplicado.?
+                alert('Hubo un problema al actualizar los datos en el servidor. ');
             }
         } catch (error) {
             console.error("Error en el PUT:", error);
@@ -200,7 +184,6 @@ if (formEditarAlumno) {
 // Evento para el boton cancelar
 if (btnCancelarEdicion) {
     btnCancelarEdicion.addEventListener('click', () => {
-        // Si se arrepiente, cerramos el modal sin hacer nada
         if (ventanaEdicion) ventanaEdicion.close();
     });
 }
